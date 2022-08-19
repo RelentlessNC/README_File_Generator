dae14bacf37522c2660721e2247b6fc0c565584e
RelentlessNC <njconklin1987@gmail.com>
2022-08-19 10:45:40 -0500

    got generateMardown.js to return content and also go the index.js writing to a file using the content returned from generateMarkdown

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,110 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const md = require('./utils/generateMarkdown.js');
+const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt([{
+                type: 'input',
+                name: 'title',
+                message: "Project title: "
+            },
+            {
+                type: 'input',
+                name: 'description',
+                message: "Project description: "
+            },
+            {
+                type: 'input',
+                name: 'installInstruction',
+                message: "Installation instructions: "
+            },
+            {
+                type: 'input',
+                name: 'usage',
+                message: "Project usage: "
+            },
+            {
+                type: 'list',
+                name: 'license',
+                message: "Project license: ",
+                choices: [{
+                    key: 'no-badge',
+                    name: 'No badge',
+                    value: 'no-badge'
+                }, {
+                    key: 'apa',
+                    name: 'Apache license 2.0',
+                    value: 'apache-2.0'
+                }, {
+                    key: 'boost',
+                    name: 'Boost Software License 1.0',
+                    value: 'boost-1.0'
+                }, {
+                    key: 'bsd3',
+                    name: 'BSD 3-Clause License',
+                    value: 'bsd-3-clause'
+                }, {
+                    key: 'bsd2',
+                    name: 'BSD 2-Clause License',
+                    value: 'bsd-2-clause'
+                }, {
+                    key: 'cc0',
+                    name: 'Creative Commons 0',
+                    value: 'cc-0-1.0'
+                }, {
+                    key: 'ccby4',
+                    name: 'Attribution 4.0 International',
+                    value: 'cc-by-4.0'
+                }, {
+                    key: 'cc-by-sa',
+                    name: 'Attribution-ShareAlike 4.0 International',
+                    value: 'cc-by-sa-4.0'
+                }]
+            },
+            {
+                type: 'input',
+                name: 'contributing',
+                message: "How to contribute: "
+            },
+            {
+                type: 'input',
+                name: 'tests',
+                message: "Tests: "
+            },
+            {
+                type: 'input',
+                name: 'questions',
+                message: "Questions: "
+            },
+        ])
+
+    .then((answers) => {
+            fs.writeFile('../README2.md', md.generateMarkdown(JSON.stringify(answers)), (err) =>
+                err ? console.log(err) : console.log(`Successfully Created`)
+            );
+
+        })
+        .catch((error) => {
+            if (error.isTtyError) {
+                // Prompt couldn't be rendered in the current environment
+            } else {
+                // Something else went wrong
+            }
+        });
+}
+
+// function writeFile(fileName, content) {
+//     fs.writeFile('README.md', md.generateMarkdown(JSON.stringify(answers)), (err) =>
+//         err ? console.log(err) : console.log(`Successfully Created ${fileName}`)
+//     );
+// }
+
+// writeFile("index.html", htmlPageContent);
+// writeFile("style.css", cssContent);
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,67 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+    switch (license) {
+        case 'no-badge':
+            return '';
+        case 'apache-2.0':
+            return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+        case 'boost-1.0':
+            return '[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)';
+            break;
+        case 'bsd-3-clause':
+            return '[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)';
+        case 'bsd-2-clause':
+            return '[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)';
+        case 'cc-0-1.0':
+            return '[![License: CC0-1.0](https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg)](http://creativecommons.org/publicdomain/zero/1.0/)';
+        case 'cc-by-4.0':
+            return '[![License: CC BY 4.0](https://img.shields.io/badge/License-CC_BY_4.0-lightgrey.svg)](https://creativecommons.org/licenses/by/4.0/)';
+        case 'cc-by-sa-4.0':
+            return '[![License: CC BY-SA 4.0](https://img.shields.io/badge/License-CC_BY--SA_4.0-lightgrey.svg)](https://creativecommons.org/licenses/by-sa/4.0/)';
+        default:
+            break;
+    }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+    switch (license) {
+        case 'no-badge':
+            return '';
+        case 'apache-2.0':
+            return '[https://opensource.org/licenses/Apache-2.0](https://opensource.org/licenses/Apache-2.0)';
+        case 'boost-1.0':
+            return '[https://www.boost.org/LICENSE_1_0.txt](https://www.boost.org/LICENSE_1_0.txt)';
+        case 'bsd-3-clause':
+            return '[https://opensource.org/licenses/BSD-3-Clause](https://opensource.org/licenses/BSD-3-Clause)';
+        case 'bsd-2-clause':
+            return '[https://opensource.org/licenses/BSD-2-Clause](https://opensource.org/licenses/BSD-2-Clause)';
+        case 'cc-0-1.0':
+            return '[http://creativecommons.org/publicdomain/zero/1.0/](http://creativecommons.org/publicdomain/zero/1.0/)';
+        case 'cc-by-4.0':
+            return '[https://creativecommons.org/licenses/by/4.0/](https://creativecommons.org/licenses/by/4.0/)';
+        case 'cc-by-sa-4.0':
+            return '[https://creativecommons.org/licenses/by-sa/4.0/)](https://creativecommons.org/licenses/by-sa/4.0/)';
+        default:
+            break;
+    }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+    const { title, description, installInstruction, usage, license, contributing, tests, questions } = JSON.parse(data);
+    return ` # ${title}
+    ${renderLicenseBadge(license)}
 
-`;
+    `;
 }
 
-module.exports = generateMarkdown;
+module.exports = { generateMarkdown };
